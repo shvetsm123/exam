c988f20f8441802a127c80a043fb9095d3e6e410
Mikhail Shvets <shvetsm123@gmail.com>
2023-11-28 18:36:03 +0200

    10 task

--- server/sandbox.sql
+++ server/sandbox.sql
@@ -9,3 +9,11 @@ SELECT role, COUNT(*)
 FROM "Users"
 GROUP BY role;
 
+// 10 task - апдейт баланса кастомеров на +10%
+UPDATE "Users"
+SET "balance" = "balance" + ("balance" * 0.1 * COALESCE((
+  SELECT COUNT(*)
+  FROM "Contests"
+  WHERE "userId" = "Users"."id" AND "createdAt" BETWEEN '2023-11-01' AND '2024-01-14'
+), 0))
+WHERE "role" = 'customer';
